64baa7db6b483c66bdf6abee05d9b3e555188dc4
chochy <radek.chocholous@gmail.com>
2019-01-10 19:03:39 +0100

    Clickable account links

--- app/src/main/java/org/esncz/esnchallenge/ProfileFragment.java
+++ app/src/main/java/org/esncz/esnchallenge/ProfileFragment.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
+import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -92,6 +93,12 @@ public class ProfileFragment extends Fragment {
         drawLogo();//TODO: Why I need to do this??
 
 //        queue = MainActivity.getQueueInstance(this.getActivity());
+
+        TextView link = (TextView) this.getActivity().findViewById(R.id.text_link_create);
+        link.setMovementMethod(LinkMovementMethod.getInstance());
+
+        link = (TextView) this.getActivity().findViewById(R.id.text_link_reset);
+        link.setMovementMethod(LinkMovementMethod.getInstance());
 
         progressBar = this.getActivity().findViewById(R.id.progress_bar);
 
